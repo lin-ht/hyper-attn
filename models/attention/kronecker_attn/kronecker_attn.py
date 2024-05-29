40f2562da71c861da1ba0e6d6dd9f9b275d09ade
lin-ht <haiting122@gmail.com>
2024-05-29 11:34:04 -0700

    add test [wip]

--- models/attention/kronecker_attn/kronecker_attn.py
+++ models/attention/kronecker_attn/kronecker_attn.py
@@ -1,7 +1,7 @@
 import random
 
-import einops
 import torch
+from attention.hyper_attn.utils import exact_attention_xformers as exact_attention
 
 
 class KroneckerAttention(torch.nn.Module):
@@ -80,10 +80,24 @@ class KroneckerAttention(torch.nn.Module):
         # concatenate the v_gps along the last dimension
         v_gp = torch.cat(v_gps, dim=-1)
 
+        # append 0 to q_gp and k_gp to match the shape of v_gp
+        zero_dim = torch.zeros(
+            q_gp.shape[:-1] + (v_gp.shape[-1] - q_gp.shape[-1],),
+            device=q_gp.device,
+            dtype=q_gp.dtype,
+        )
+        q_gp_ = torch.cat([q_gp, zero_dim], dim=-1)
+        zero_dim = torch.zeros(
+            k_gp.shape[:-1] + (v_gp.shape[-1] - k_gp.shape[-1],),
+            device=q_gp.device,
+            dtype=q_gp.dtype,
+        )
+        k_gp_ = torch.cat([k_gp, zero_dim], dim=-1)
+
         # attn_gps shape = [batch_size, head_size, n_gp[0], dim * n_key_groups]
         # lsp_gp shape = [batch_size, head_size, n_gp[0], 1]
         attn_rst = self.attn_calculator(
-            q_gp, k_gp, v_gp, scale, causal=False, return_lse=return_lse
+            q_gp_, k_gp_, v_gp, scale, causal=False, return_lse=return_lse
         )
 
         if not return_lse:
@@ -96,14 +110,15 @@ class KroneckerAttention(torch.nn.Module):
         k_picked = random.randint(0, n_gp[1] - 1)
         k_sample = key[:, k_picked :: n_gp[1], :, :]
 
-        q_ = einops.rearrange(query, "b l h d -> (b h) l d")
-        k_ = einops.rearrange(k_sample, "b l h d -> (b h) d l")
+        q_ = query.reshape([-1, n_query, dim])
+        # k_ = einops.rearrange(k_sample, "b h l d -> (b h) d l")
+        k_ = k_sample.reshape([q_.shape[0], -1, dim]).transpose(-1, -2)
         w_ = torch.bmm(q_, k_).reshape(batch_size, head_size, n_query, -1)
-        w_gps = torch.cat(value.chunk(w_, dim=-2), dim=-1).transpose(-1, -2)
+        w_gps = torch.cat(w_.chunk(n_query_groups, dim=-2), dim=-1).transpose(-1, -2)
         w_gps = w_gps.reshape(batch_size, head_size, n_query_groups, n_key_groups, -1)
         norm_gps = w_gps.norm(p=2, dim=-1)
         # s_gps shape = [batch_size, head_size, n_query_groups, n_key_groups]
-        s_gps = norm_gps / norm_gps[..., c_gp[0], c_gp[1]]
+        s_gps = norm_gps / norm_gps[..., c_gp[0], c_gp[1]].unsqueeze_(-1).unsqueeze_(-1)
 
         # 3. Compute the kronecker product of S and B
         # attn_gps shape = [batch_size, head_size, 1, n_gp[0], n_key_groups, dim]
@@ -123,3 +138,80 @@ class KroneckerAttention(torch.nn.Module):
             # lsp_gp shape = [batch_size, head_size, n_gp[0], 1]
             lse = lse_gp.unsqueeze_(2) + torch.log(s_gps_sum.squeeze_(-1))
             return attn, lse
+
+
+if __name__ == "__main__":
+    batch_size = 2
+    head_size = 4
+    n_query = 8
+    n_key = 8
+    dim = 16
+    n_query_groups = 2
+    n_key_groups = 2
+
+    n_gp = [n_query // n_query_groups, n_key // n_key_groups]
+    c_gp = [n_query_groups // 2, n_key_groups // 2]
+
+    q_gp = torch.randn(
+        batch_size, head_size, n_gp[0], dim, dtype=torch.bfloat16, device="cuda"
+    )
+    k_gp = torch.randn(
+        batch_size, head_size, n_gp[1], dim, dtype=torch.bfloat16, device="cuda"
+    )
+    s_q = (
+        torch.rand(
+            batch_size,
+            head_size,
+            n_query_groups,
+            1,
+            dtype=torch.bfloat16,
+            device="cuda",
+        )
+        * 2
+        + 0.1
+    )
+    s_k = (
+        torch.rand(
+            batch_size, head_size, n_key_groups, 1, dtype=torch.bfloat16, device="cuda"
+        )
+        * 2
+        + 0.1
+    )
+    s_q[..., c_gp[0], :] = 1.0
+    s_k[..., c_gp[1], :] = 1.0
+    s_gps = s_q * s_k.transpose(-1, -2)
+    print(f"s_gps[0,0] = {s_gps[0,0]}")
+
+    query = (q_gp.unsqueeze_(2) * s_q.unsqueeze_(-1)).reshape(
+        batch_size, head_size, n_query, dim
+    )
+    print(f"query[0,0] = {query[0,0]}")
+    key = (k_gp.unsqueeze_(2) * s_k.unsqueeze_(-1)).reshape(
+        batch_size, head_size, n_key, dim
+    )
+    print(f"key[0,0] = {key[0,0]}")
+    value = torch.randn(
+        batch_size, head_size, n_key, dim, dtype=torch.bfloat16, device="cuda"
+    )
+
+    def attn_calculator_func(query, key, value, scale, causal=False, return_lse=False):
+        attn, lse = exact_attention(query, key, value, scale, causal)
+        if return_lse:
+            return attn, lse
+        else:
+            return attn
+
+    attn_module = KroneckerAttention(attn_calculator_func)
+
+    scale = dim ** (-0.5)
+    attn_approx, lse_approx = attn_module(
+        query, key, value, n_query_groups, n_key_groups, scale, return_lse=True
+    )
+    print(f"attn_approx[0,0] = {attn_approx[0,0]}")
+    print(f"lse_approx[0,0] = {lse_approx[0,0]}")
+    attn_exact, lse_exact = attn_calculator_func(query, key, value, scale, False, True)
+    print(f"attn_exact[0,0] = {attn_exact[0,0]}")
+    print(f"lse_exact[0,0] = {lse_exact[0,0]}")
+
+    torch.testing.assert_allclose(attn_approx, attn_exact, rtol=1e-3, atol=1e-3)
+    torch.testing.assert_allclose(lse_approx, lse_exact, rtol=1e-3, atol=1e-3)
